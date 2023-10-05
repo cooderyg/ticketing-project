518afa004224c6a738a18b5bcb540abf48f73150
cooderyg <ygk9604@naver.com>
2023-10-05 20:24:55 +0900

    docs: profile-upload swagger response profile 업로드 swagger response 추가

--- src/apis/uploads/uploads.controller.ts
+++ src/apis/uploads/uploads.controller.ts
@@ -4,11 +4,16 @@ import { UploadsService } from './uploads.service';
 import { UploadFileResDto } from './dto/res.dto';
 import { AccessAuthGuard } from '../auth/guard/auth.guard';
 import { User, UserAfterAuth } from 'src/commons/decorators/user.decoreator';
+import { ApiPostResponse } from 'src/commons/decorators/swagger.decorator';
+import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 
+@ApiExtraModels(UploadFileResDto)
+@ApiTags('uploads')
 @Controller('uploads')
 export class UploadsController {
   constructor(private readonly uploadsService: UploadsService) {}
 
+  @ApiPostResponse(UploadFileResDto)
   @UseGuards(AccessAuthGuard)
   @Post('profile-image')
   @UseInterceptors(FileInterceptor('file'))
